0b5868c2cea56b3edff7026291511f7f40536609
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-05-10 21:18:30 +0530

    Created basic health check

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, routing::get};
+use serde::Serialize;
+use std::net::SocketAddr;
+
+#[derive(Serialize)]
+struct Status {
+    message: String,
+    status: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/health", get(health_check));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> Json<Status> {
+    Json(Status {
+        message: "Battlefront Game Online".to_string(),
+        status: "OK".to_string(),
+    })
 }
