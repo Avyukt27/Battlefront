949dda9f7d7c4acbb02f1261a96f44c71d1c913a
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-05-11 17:14:23 +0530

    Added some QoL

--- frontend/src/components/game/GameControls.vue
+++ frontend/src/components/game/GameControls.vue
@@ -3,6 +3,9 @@ import { useGameStore } from '@/stores/game';
 import { computed } from 'vue';
 
 const store = useGameStore();
+
+const myPlayer = computed(() => store.gameState?.players.find((p) => p.id === store.myPlayerId));
+const isMyTurn = computed(() => store.gameState?.current_turn === myPlayer.value?.colour);
 const roll = computed(() => store.gameState?.last_roll);
 </script>
 
@@ -14,8 +17,9 @@ const roll = computed(() => store.gameState?.last_roll);
     </button>
     <button
       class="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 text-white font-bold rounded-md shadow-lg shadow-indigo-500/20 transition-transform active:scale-95"
-      @click="store.rollDice" :disabled="store.isRolling">
-      <span v-if="store.isRolling">Rolling...</span>
+      @click="store.rollDice" :disabled="store.isRolling || roll !== 0 || !isMyTurn">
+      <span v-if="store.isRolling && roll === 0">Rolling...</span>
+      <span v-else-if="roll !== 0">Waiting for {{ store.gameState?.current_turn }}</span>
       <span v-else>Roll Dice</span>
     </button>
     <button @click="store.leaveGame"
@@ -23,5 +27,5 @@ const roll = computed(() => store.gameState?.last_roll);
       Leave Game
     </button>
   </div>
-  <div v-if="roll != 0" class="text-xl text-indigo-400">Roll: {{ roll }}</div>
+  <div v-if="roll !== 0 && isMyTurn" class="text-xl text-indigo-400">Roll: {{ roll }}</div>
 </template>
